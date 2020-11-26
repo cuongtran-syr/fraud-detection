ff70688d9ef5bdd76ec62bdc99b76185bab93744
cuongtran-syr <cuong.tranus@gmail.com>
2020-11-26 16:33:43 -0500

    remove big data

---
+++ codes/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.stats import entropy
+from math import log, e
+import pandas as pd
+
+def pandas_entropy(column, base=None):
+  """
+  Compute the entropy for a categorical column in a pandas table
+  Example: pandas_entropy(pd00['merchantName']
+  
+  """
+  vc = pd.Series(column).value_counts(normalize=True, sort=False)
+  base = e if base is None else base
+  return -(vc * np.log(vc)/np.log(base)).sum()
